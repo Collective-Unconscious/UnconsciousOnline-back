33ceada38251fe7415d99c7a1480ead9cb2e2a54
Noah Ravaux <ravaux.noah@gmail.com>
2026-05-27 11:53:33 +0200

    fix: allow parsing of the @exclude decorator

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
